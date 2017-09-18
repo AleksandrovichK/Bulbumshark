64151de15224b7efbb5eb48eb752d259dfc068c9
Cyryl Bulbum <killertom1997@gmail.com>
2017-09-18 20:03:31 +0300

    The last place where ARP still exist

--- src/com/company/Data.java
+++ src/com/company/Data.java
@@ -2,14 +2,14 @@ package com.company;
 
 import java.awt.*;
 
-/**
+/*
  * Created by bulbum on 04.09.2017.
  * This register controls the main parameters of the program.
  */
 class Data {
         final static int frameHeight = 800; //высота окна
         final static int frameWidth = 800; //ширина окна
-        final static int leftIndent = 20; //размещение окна на дисплее, отступ слева,  в процентах
+        final static int leftIndent = 25; //размещение окна на дисплее, отступ слева,  в процентах
         final static int upIndent = 2;   //размещение окна на дисплее, отступ сверху, в процентах
         final static int uniIndent = 20; //юнион отступ
 
@@ -18,10 +18,12 @@ class Data {
 
         final static CRectangle AllPacketsBounds = new CRectangle(uniIndent, uniIndent,frameWidth-uniIndent*2-4,280);  //расположение области пути
         final static CRectangle StackProtocolsBounds = new CRectangle(uniIndent,AllPacketsBounds.getHeight()+AllPacketsBounds.getTop()*3,20*frameWidth/100-uniIndent*2,frameHeight-(AllPacketsBounds.getTop() + AllPacketsBounds.getHeight()+4*uniIndent));  //расположение области пути
-        final static CRectangle ActivePacketsBounds = new CRectangle(StackProtocolsBounds.getLeft()+StackProtocolsBounds.getWidth()+2*uniIndent, frameHeight-2*uniIndent-160,50*frameWidth/100-uniIndent*2,160);  //расположение области пути
 
         final static CRectangle trashKBounds = new CRectangle(frameWidth-uniIndent-24, AllPacketsBounds.getTop()+AllPacketsBounds.getHeight(), 20, 20);
         final static CRectangle searchKBounds = new CRectangle(trashKBounds.getLeft()-21, trashKBounds.getTop(), trashKBounds.getWidth(), trashKBounds.getHeight());
         final static CRectangle addKBounds = new CRectangle(StackProtocolsBounds.getWidth()+StackProtocolsBounds.getLeft()+uniIndent, AllPacketsBounds.getTop()+AllPacketsBounds.getHeight()+uniIndent*2, 21, 21);
+        final static CRectangle clStackKBounds = new CRectangle(addKBounds.getLeft(), addKBounds.getTop()+addKBounds.getHeight()+5, addKBounds.getWidth(),addKBounds.getHeight());
         final static CRectangle comboPBounds = new CRectangle(addKBounds.getLeft()+addKBounds.getWidth()+uniIndent,addKBounds.getTop(), 240,30);  //расположение кнопки net user
+        final static CRectangle ActivePacketsBounds = new CRectangle(StackProtocolsBounds.getLeft()+StackProtocolsBounds.getWidth()+2*uniIndent, frameHeight-2*uniIndent-260,frameWidth-addKBounds.getLeft()-addKBounds.getWidth()-uniIndent-4,260);  //расположение области пути
+        final static CRectangle filterKBounds = new CRectangle(ActivePacketsBounds.getLeft()+ActivePacketsBounds.getWidth()-100, ActivePacketsBounds.getTop()-100-uniIndent, 100, 100);
 }
--- src/com/company/View.java
+++ src/com/company/View.java
@@ -4,28 +4,15 @@ import io.kaitai.struct.KaitaiStream;
 
 import java.io.*;
 
-/*
-import org.jnetpcap.Pcap;
-import org.jnetpcap.PcapIf;
-import org.jnetpcap.packet.PcapPacket;
-import org.jnetpcap.packet.PcapPacketHandler;
-import org.jnetpcap.protocol.lan.Ethernet;
-import org.jnetpcap.protocol.network.Arp;
-import org.jnetpcap.protocol.network.Ip4;
-import org.jnetpcap.protocol.tcpip.Http;
-import org.jnetpcap.protocol.tcpip.Tcp;
-import org.jnetpcap.protocol.tcpip.Udp;
-*/
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Vector;
 
 class View extends JFrame {
-
     private JScrollPane upArea;
     private JScrollPane downArea;
 
@@ -33,17 +20,18 @@ class View extends JFrame {
     private JTextArea ActivePackets;
     private JTextArea StackProtocols;
 
+    private Vector <String> vectorOfProtocols;
+
     private JComboBox<String> comboProtocols;
+    private String[] protocols = {"Ethernet", "ARP", "IPv4", "IPv6","TCP", "UDP","HTTP", "DNS", "SSL/TLS"};
 
-    private JTextField portField;
-    private int port;
     private int iter = 0;
-    private StringBuilder filtered;
 
-    private JLabel portLabel;
     private JButton searchKey;
     private JButton addKey;
     private JButton trashKey;
+    private JButton filterKey;
+    private JButton clearStackKey;
 
     View() throws HeadlessException, IOException {
         settings();
@@ -51,9 +39,12 @@ class View extends JFrame {
         this.add(upArea);
         this.add(downArea);
         this.add(StackProtocols);
+        this.add(comboProtocols);
         this.add(searchKey);
         this.add(addKey);
+        this.add(clearStackKey);
         this.add(trashKey);
+        this.add(filterKey);
     }
 
     private void settings() throws IOException {
@@ -61,7 +52,7 @@ class View extends JFrame {
         this.setIconImage(ImageIO.read(new File("src//com//company//pics//icon.jpg")));
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("src//com//company//pics//back_2.jpg")))));
+        this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("src//com//company//pics//back_upd.jpg")))));
         this.setBounds(Data.leftIndent * Toolkit.getDefaultToolkit().getScreenSize().width / 100, Data.upIndent * Toolkit.getDefaultToolkit().getScreenSize().height / 100, Data.frameWidth, Data.frameHeight);
         this.setLayout(null);
 
@@ -94,7 +85,7 @@ class View extends JFrame {
 
         StackProtocols = new JTextArea();
         StackProtocols.setBackground(Data.centralColor);
-        StackProtocols.setFont(Data.centralFontInAreas);
+        StackProtocols.setFont(new Font("Microsoft JhengHei Light", Font.BOLD, 13));
         StackProtocols.setAutoscrolls(true);
         StackProtocols.setBorder(BorderFactory.createCompoundBorder());
         StackProtocols.setLineWrap(true);
@@ -102,16 +93,11 @@ class View extends JFrame {
         StackProtocols.setEditable(false);
         StackProtocols.setBounds(Data.StackProtocolsBounds.getLeft(), Data.StackProtocolsBounds.getTop(), Data.StackProtocolsBounds.getWidth(), Data.StackProtocolsBounds.getHeight());
 
-        comboProtocols = new JComboBox<>(new String[]{"Ethernet", "ARP", "IPv4", "IPv6","TCP", "UDP","HTTP", "DNS", "SSL/TLS"});
+        comboProtocols = new JComboBox<>(protocols);
         comboProtocols.addItemListener(e -> {
             if (e.getStateChange() == ItemEvent.SELECTED) {
-                if (e.getItem().toString().equals("ARP")) System.out.println("Arp");
-                    //toMakeOnlyARP();
-                //else toMakeOnlyEthernet();
-
+                //if (e.getItem().toString().equals("ARP")) System.out.println("Arp");
             }
-            ;
-
         });
         comboProtocols.setBounds(Data.comboPBounds.getLeft(), Data.comboPBounds.getTop(), Data.comboPBounds.getWidth(), Data.comboPBounds.getHeight());
 
@@ -130,10 +116,11 @@ class View extends JFrame {
 
             @Override
             public void mouseReleased(MouseEvent e) {
+                AllPackets.setBackground(Color.WHITE);
                 searchKey.setBounds(searchKey.getX() - 2, searchKey.getY() - 2, searchKey.getWidth() + 2, searchKey.getHeight() + 2);
 
                 try {
-                    toParseFromFile();
+                    toLookPreview();
 
                 } catch (IOException e1) {
                     e1.printStackTrace();
@@ -151,7 +138,7 @@ class View extends JFrame {
             }
         });
 
-        trashKey = new JButton(new ImageIcon("src//com//company//pics//trash_2.png"));
+        trashKey = new JButton(new ImageIcon("src//com//company//pics//trash.png"));
         trashKey.setBounds(Data.trashKBounds.getLeft(), Data.trashKBounds.getTop(), Data.trashKBounds.getWidth(), Data.trashKBounds.getHeight());
         trashKey.addMouseListener(new MouseListener() {
             @Override
@@ -168,6 +155,7 @@ class View extends JFrame {
             public void mouseReleased(MouseEvent e) {
                 trashKey.setBounds(trashKey.getX() - 2, trashKey.getY() - 2, trashKey.getWidth() + 2, trashKey.getHeight() + 2);
                 AllPackets.setText("");
+                AllPackets.setBackground(Data.centralColor);
                 iter=0;
             }
 
@@ -184,7 +172,7 @@ class View extends JFrame {
 
         addKey = new JButton(new ImageIcon("src//com//company//pics//add.png"));
         addKey.setBounds(Data.addKBounds.getLeft(), Data.addKBounds.getTop(), Data.addKBounds.getWidth(), Data.addKBounds.getHeight());
-        addKey.addMouseListener(new MouseListener() {
+        addKey.addMouseListener(new MouseListener(){
             @Override
             public void mouseClicked(MouseEvent e) {
 
@@ -197,8 +185,10 @@ class View extends JFrame {
 
             @Override
             public void mouseReleased(MouseEvent e) {
+                StackProtocols.setBackground(Color.WHITE);
                 addKey.setBounds(addKey.getX() - 5, addKey.getY() - 5, addKey.getWidth() + 5, addKey.getHeight() + 5);
-                toShowWhatToAdd();
+                StackProtocols.append(""+comboProtocols.getSelectedItem()+'\n');
+                vectorOfProtocols.add(comboProtocols.getSelectedItem().toString());
             }
 
             @Override
@@ -212,32 +202,88 @@ class View extends JFrame {
             }
         });
 
-        filtered = new StringBuilder();
+        clearStackKey = new JButton(new ImageIcon("src//com//company//pics//clearstack.png"));
+        clearStackKey.setBounds(Data.clStackKBounds.getLeft(), Data.clStackKBounds.getTop(), Data.clStackKBounds.getWidth(), Data.clStackKBounds.getHeight());
+        clearStackKey.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                clearStackKey.setBounds(clearStackKey.getX() + 5, clearStackKey.getY() + 5, clearStackKey.getWidth() - 5, clearStackKey.getHeight() - 5);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                StackProtocols.setBackground(Data.centralColor);
+                clearStackKey.setBounds(clearStackKey.getX() - 5, clearStackKey.getY() - 5, clearStackKey.getWidth() + 5, clearStackKey.getHeight() + 5);
+                StackProtocols.setText("");
+                vectorOfProtocols.clear();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        filterKey = new JButton(new ImageIcon("src//com//company//pics//filter.png"));
+        filterKey.setBounds(Data.filterKBounds.getLeft(), Data.filterKBounds.getTop(), Data.filterKBounds.getWidth(), Data.filterKBounds.getHeight());
+        filterKey.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                filterKey.setBounds(filterKey.getX() + 2, filterKey.getY() + 2, filterKey.getWidth() - 2, filterKey.getHeight() - 2);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                ActivePackets.setBackground(Color.WHITE);
+                filterKey.setBounds(filterKey.getX() - 2, filterKey.getY() - 2, filterKey.getWidth() + 2, filterKey.getHeight() + 2);
+
+                try {toFiltrate();} catch (IOException e1) {e1.printStackTrace();}
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        vectorOfProtocols = new Vector<>();
 
         this.setVisible(true);
     }
-
-    private void toShowWhatToAdd(){
-        if (!comboProtocols.isShowing()) this.add(comboProtocols);
-        this.repaint();
-    }
-    private void toParseFromFile() throws IOException {
-        AllPackets.setBackground(Color.WHITE);
-        ActivePackets.setBackground(Color.WHITE);
-        //Vector <Pcap.Packet> packets = new Vector<>();
-
+    private void toLookPreview() throws IOException {
         EthernetFrame frame;
         Ipv4Packet ipv4;
         Ipv6Packet ipv6;
         TcpSegment tcp;
         UdpDatagram udp;
+        DnsPacket dns;
+        TlsClientHello tls;
 
         Pcap p = Pcap.fromFile("material/log4MB.pcap");
         KaitaiStream io = p._io();
 
         while (!io.isEof()) {
             Pcap.Packet packet = new Pcap.Packet(io, p, p);
-            //packets.add(packet);
 
             frame = (EthernetFrame) packet.body();
 
@@ -259,8 +305,90 @@ class View extends JFrame {
                 default: {break;}
             }
         }
-        //System.out.println("ALL PACKETS HAD BEEN READ: "+packets.size());
+        System.out.println("ALL PACKETS HAD BEEN READ: ");
         }
+    private void toFiltrate() throws IOException {
+        iter = 0;
+
+        EthernetFrame frame = null;
+        Ipv4Packet ipv4 = null;
+        Ipv6Packet ipv6 = null;
+        TcpSegment tcp = null;
+        UdpDatagram udp = null;
+        DnsPacket dns = null;
+        TlsClientHello tls = null;
+
+        Pcap p = Pcap.fromFile("material/log4MB.pcap");
+        KaitaiStream io = p._io();
+
+        while (!io.isEof()) {
+            Pcap.Packet packet = new Pcap.Packet(io, p, p);
+            Object container = packet.body();
+            frame = (EthernetFrame) container;
+
+            if (!frame.body().getClass().getSimpleName().equals("Ipv4Packet")) System.out.println(frame.body().getClass().getSimpleName());
+
+
+            for (int protIter = 0; protIter < vectorOfProtocols.size(); protIter++) {
+                switch (vectorOfProtocols.get(protIter)) {
+                    case "Ethernet": {
+                            if (container.getClass().getSimpleName().equals("EthernetFrame")){
+                        //        System.out.println(container.getClass().getSimpleName());
+                                ActivePackets.append(protIter==1? ++iter+". Ethernet": " -> Ethernet");
+                                ActivePackets.append(protIter == vectorOfProtocols.size()? "\n" : "");
+
+                                frame = (EthernetFrame) container;
+                                container = frame.body();
+                          //      System.out.println(container.getClass().getSimpleName());
+                            }
+                        break;
+                    }
+                    case "ARP": {
+                        if (container.getClass().getSimpleName().equals("ARP")){
+                            ActivePackets.append(protIter==1? ++iter+". ARP": " -> ARP");
+                            ActivePackets.append(protIter == vectorOfProtocols.size()? "\n" : "");
+
+                            //ARP class doesn't exist!
+                            frame = (EthernetFrame) container;
+                            container = frame.body();
+                        }
+                        break;
+                    }
+                    case "IPv4": {
+                        if (null != frame)
+                            ipv4 = (Ipv4Packet) frame.body();
+                        ActivePackets.append(" -> IPv4");
+                        break;
+
+                    }
+                    case "IPv6": {
+
+                    }
+                    case "TCP": {
+                        if (null != ipv4)
+                            System.out.println(ipv4.protocol());
+
+                    }
+                    case "UDP": {
+
+                    }
+                    case "HTTP": {
+
+                    }
+                    case "DNS": {
+
+                    }
+                    case "SSL/TLS": {
+                    }
+                }
+
+            }
+        }
+
+//                                ipv6 = (Ipv6Packet) frame.body();
+  //                              ActivePackets.append("Packet #" + (++iter) + ": Ehternet -> IPv6 -> " + ipv6.nextHeader().getClass().getSimpleName() + "\n");
+
+    }
 }
 /*
         private void toMakeOnlyARP() {
